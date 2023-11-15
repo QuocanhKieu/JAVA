007c065c9d01e0701e1e89304705e2742984cb8f
QuocanhKieu <kieuquocanh4@gmail.com>
2023-11-15 20:13:35 +0700

    ff

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,53 @@ public class Main{
         System.out.println("Hello World!");
 
         double t = 15.15;
-        sayHello("Everybody");
+//        sayHello("Everybody");
 
+//        isNguyenTo(8);
 
+//        System.out.println(findFiboN(7));
 
-        System.out.println();
-
-        isNguyenTo(8);
+        System.out.println(findNearestPrimeOf(8));
     }
     static void sayHello(String msg) {
         System.out.println("hello" + msg );
     }
 
-    static void isNguyenTo(int num) {
+    static boolean isNguyenTo(int num) {
         if (num < 1) {
-            System.out.println("Ko La So Nguyen To");
-            return;
+            return false;
         }
 
         for( int i = 2; i <=(int)(Math.sqrt(num)) ;i++) {
             if (num % i == 0 ) {
-                System.out.println("Ko La So Nguyen To");
-                return;
+                return false;
             }
 
         }
-        System.out.println("La So Nguyen To");
+        return true;
+
+    }
+
+    static int findFiboN(int order) {
+        if (order == 0) return 0;
+        if (order == 1) return 1;
+        int pre = 0;
+        int cur = 1;
+        for (int i = 0; i < order -1 ; i++)
+        {
+            int temp = cur;
+            cur = cur + pre;
+            pre = temp;
+        }
+        return cur;
+    }
+
+    static int findNearestPrimeOf(int num) {
+        for( int i = num- 1 ; i >= 2; i--) {
+
+            if (isNguyenTo(i))
+                return i;
+        }
+        return 2;
     }
 }
